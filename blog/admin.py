dcafc10e190a6cca19a70706f46ee2d231e31f2e
Esraa Saad <esraamohamed4037@gmail.com>
2024-07-18 09:40:55 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -9,4 +9,5 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Tag)
+admin.site.register(Comment)
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
-
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 
 
 class Tag(models.Model):
@@ -8,6 +9,18 @@ class Tag(models.Model):
 
   def __str__(self):
     return self.value
+
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    # Using GenericForeignKey to use the comment with the post and the auther
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
 
 
 class Post(models.Model):
@@ -20,6 +33,7 @@ class Post(models.Model):
   summary = models.TextField(max_length=500)
   content = models.TextField()
   tags = models.ManyToManyField(Tag, related_name="posts")
+  comments = GenericRelation(Comment)
 
   def __str__(self):
       return self.title
